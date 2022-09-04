ddfcf9df36284ac7eba9c91a4ddae197cc68f471
xhemj <xhemj2680@163.com>
2022-09-04 21:23:30 +0800

    预览时暂时移除日期等信息

--- admin/preview.js
+++ admin/preview.js
@@ -6,29 +6,30 @@ let PostPreview = createClass({
       { className: "preview-body" },
       h("header", {}, [
         h("h1", {}, entry.getIn(["data", "title"])),
-        h("div", { className: "post-meta" }, [
-          h("div", { className: "date" }, [
-            h("span", { className: "posted-on" }, [
-              h("i", { className: "fa fa-calendar" }),
-              h("time", {}, entry.getIn(["data", "date"])),
-            ]),
-            h("span", { className: "reading-time" }, [
-              h("i", { className: "fa fa-clock" }),
-              h("span", {}, "阅读时间：XX 分钟"),
-            ]),
-          ]),
-          h("div", { className: "tags" }, [
-            h("i", { className: "fa fa-tag" }),
-            h("span", { className: "tag" }, [
-              h("a", {}, entry.getIn(["data", "tags"])),
-            ]),
-          ]),
-        ]),
+        // h("div", { className: "post-meta" }, [
+        //     h("div", { className: "date" }, [
+        //       h("span", { className: "posted-on" }, [
+        //         h("i", { className: "fa fa-calendar" }),
+        //         h("time", {}, entry.getIn(["data", "date"])),
+        //       ]),
+        //       h("span", { className: "reading-time" }, [
+        //         h("i", { className: "fa fa-clock" }),
+        //         h("span", {}, "阅读时间：XX 分钟"),
+        //       ]),
+        //     ]),
+        //     h("div", { className: "tags" }, [
+        //       h("i", { className: "fa fa-tag" }),
+        //       h("span", { className: "tag" }, [
+        //         h("a", {}, entry.getIn(["data", "tags"])),
+        //       ]),
+        //     ]),
+        // ]),
       ]),
       h("div", { className: "text" }, this.props.widgetFor("body"))
     );
   },
 });
+NetlifyCms.registerPreviewTemplate("posts", PostPreview);
 
 NetlifyCms.registerEditorComponent({
   // Internal id of the component
